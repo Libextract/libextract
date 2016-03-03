98d13687ddaaa4c77abac9bc5e9046db70fe93cb
Luca Soldaini <luca@soldaini.net>
2016-03-02 22:44:00 -0500

    added example to motivate change

--- libextract/core.py
+++ libextract/core.py
@@ -24,7 +24,15 @@ def parse_html(fileobj, encoding):
     The *encoding* is assumed to be utf8.
     """
     parser = HTMLParser(encoding=encoding, remove_blank_text=True)
-    return etree.fromstring(fileobj, parser)
+
+    try:
+        # this is done because lxml sometimes (yes, only sometimes)
+        # does not like the fact that fileobj is a string and not a file
+        # object. See http://stackoverflow.com/a/8917972 for more info.
+        # note that, if this method does not work, we fallback to parse.
+        return etree.fromstring(fileobj, parser)
+    except ValueError:
+        return parse(fileobj, parser)
 
 
 def pipeline(data, funcs):
